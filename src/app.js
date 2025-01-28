4a6007e5e192e0b15bf0e48118f7b0939908dcbd
ryuu000 <99164869+ryuu000@users.noreply.github.com>
2025-01-28 15:16:13 +0530

    Create app.js
    Signed-off-by: ryuu000 <99164869+ryuu000@users.noreply.github.com>

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSection from './HeroSection';
+import GrapeEditor from './GrapeEditor';
+
+const App = () => (
+  <div>
+    <HeroSection />
+    <GrapeEditor />
+  </div>
+);
+
+export default App;
